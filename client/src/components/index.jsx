97f55f7127d431ce88cc55d3b5c3495600d81f3f
suraj.a <suraj02anand@gmail.com>
2024-02-28 17:52:23 +0530

    organize generic components

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,7 +1,7 @@
-import { Text } from "./Text";
-import { Heading } from "./Heading";
-import { Img } from "./Img";
-import { Button } from "./Button";
-import { Input } from "./Input";
-import { TextArea } from "./TextArea";
+import { Text } from "./generic/Text";
+import { Heading } from "./generic/Heading";
+import { Img } from "./generic/Image";
+import { Button } from "./generic/Button";
+import { Input } from "./generic/Input";
+import { TextArea } from "./generic/TextArea";
 export { Text, Heading, Img, Button, Input, TextArea };
