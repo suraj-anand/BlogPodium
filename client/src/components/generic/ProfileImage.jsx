4829faec61ed74a503fb00ef5d942537bc73d9ea
suraj.a <suraj02anand@gmail.com>
2024-03-10 01:20:15 +0530

    default profile

--- client/src/components/generic/ProfileImage.jsx
+++ client/src/components/generic/ProfileImage.jsx
@@ -1,13 +1,17 @@
 import { Img } from "./Image"
+import { FaRegUserCircle } from "react-icons/fa";
 
 export const ProfileImage = ({
-  imgSrc="assets/img_ellipse_5.png", className
+  imgSrc="", className
 }) => {
-  return (
-    <Img
-      src={imgSrc}
-      alt="circleimage"
-      className={`h-[70px] w-[70px] rounded-[50%] ${className}`}
-    />
-  )
+  
+  if(imgSrc)
+    return (
+      <Img
+        src={imgSrc}
+        alt="circleimage"
+        className={`h-[70px] w-[70px] rounded-[50%] ${className}`}
+      />
+    )
+  return <FaRegUserCircle size={36} />
 }
